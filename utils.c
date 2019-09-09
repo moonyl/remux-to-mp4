4b79135a2f75f97ab74e54d3bdb0ed8b373959af
moonyl <moonysl@hanmail.net>
2019-09-09 14:06:53 +0900

    initial commit

---
+++ utils.c
@@ -0,0 +1,22 @@
+//
+// Created by admin on 2019-08-28.
+//
+
+#include <libavutil/timestamp.h>
+#include <libavformat/avformat.h>
+
+void log_packet(const AVFormatContext *fmt_ctx, const AVPacket *pkt, const char *tag)
+{
+    AVRational *time_base = &fmt_ctx->streams[pkt->stream_index]->time_base;
+    printf("%s: pts:%s pts_time:%s dts:%s dts_time:%s duration:%s duration_time:%s stream_index:%d\n",
+           tag,
+           av_ts2str(pkt->pts), av_ts2timestr(pkt->pts, time_base),
+           av_ts2str(pkt->dts), av_ts2timestr(pkt->dts, time_base),
+           av_ts2str(pkt->duration), av_ts2timestr(pkt->duration, time_base),
+           pkt->stream_index);
+}
+
+char* av_error2char(int errorNum)
+{
+    return av_err2str(errorNum);
+}
